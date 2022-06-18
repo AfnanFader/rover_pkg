9f2d4c7a5a220d5c5e9ba3766eac6a9437548054
AfnanFader <afnanfader@gmail.com>
2022-06-19 01:18:11 +0800

    gimbal-arduino code update

--- src/Arduino/gimbal-arduino/gimbal-arduino.ino
+++ src/Arduino/gimbal-arduino/gimbal-arduino.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <ros.h>
 #include <Servo.h>
-#include <std_msgs/UInt16MultiArray.h>
+#include <rover_pkg/servo.h>
 #include <std_msgs/UInt8.h>
 
 // Define I/O channel pins
@@ -10,10 +10,36 @@ static const int SERVO_TILT_PIN = 2;
 static const int LINEAR_UP_PIN = 3;
 static const int LINEAR_DOWN_PIN = 4;
 
-// Servo control callback for ROS Node
-void servoControlCallback(const std_msgs::UInt16MultiArray& cmd_msg) {
+// Servo Objects
+Servo tiltServo;
+Servo panServo;
 
+bool calculate_servo_location(uint8_t& curr_pos, int new_val) {
+
+    bool update = false;
+    uint8_t update_val = curr_pos + new_val;
+
+    if (update_val >= 0 || update_val <= 180) {
+        update = true;
+    }
+
+    return update;
+}
+
+// Servo control callback for ROS Node
+void servoControlCallback(const rover_pkg::servo& cmd_msg) {
     
+    // Read current position
+    uint8_t curr_pan_val = panServo.read();
+    uint8_t curr_tilt_val = tiltServo.read();
+
+    if (calculate_servo_location(curr_pan_val, cmd_msg.pan)) {
+        panServo.write(cmd_msg.pan);
+    }
+
+    if (calculate_servo_location(curr_tilt_val, cmd_msg.tilt)) {
+        tiltServo.write(cmd_msg.tilt);
+    }
 
 }
 
@@ -37,7 +63,7 @@ void linearControlCallback(const std_msgs::UInt8& cmd_msg) {
 
     if (cmd_msg.data == 128) {
 
-        digitalWrite(LINEAR_UP_PIN, HIGH);
+        digitalWrite(LINEAR_UP_PIN, LOW);
         digitalWrite(LINEAR_DOWN_PIN, LOW);
 
     }
@@ -53,7 +79,7 @@ ros::NodeHandle nh;
  * 0 - Pan --> 0 - 180 degree
  * 1 - Tilt --> 0 - 180 degree
  */
-ros::Subscriber<std_msgs::UInt16MultiArray> servoControlSubscriber("servo_control", servoControlCallback);
+ros::Subscriber<rover_pkg::servo> servoControlSubscriber("servo_control", servoControlCallback);
 
 /*!
  * \brief Motor commands cmd in from the pi in an UInt8MultiArray.
@@ -62,10 +88,6 @@ ros::Subscriber<std_msgs::UInt16MultiArray> servoControlSubscriber("servo_contro
  * DOWN - 0
  */
 ros::Subscriber<std_msgs::UInt8> linearControlSubscriber("linear_control", linearControlCallback);
-
-// Servo Objects
-Servo tiltServo;
-Servo panServo;
 
 void setup() {
 
